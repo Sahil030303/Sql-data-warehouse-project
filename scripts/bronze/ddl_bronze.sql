454262eebb6f64cfd54e119803bbcb0b9121ca13
Sahil030303 <91740934+Sahil030303@users.noreply.github.com>
2026-01-09 20:51:26 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,128 +1,90 @@
 /*
-=====================================================
-
-DDL Script : Create Bronze Tables
-
-=====================================================
-
-Script Purpose : 
-    This script creates tables in the 'bronze' schema, dropping existing tables
-    if they already exist  
-    Run this script to re-define the DDL structure of 'bronze' tables
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME
-	BEGIN TRY
-		SET @batch_start_time = GETDATE()
-		PRINT('=======================================');
-		PRINT('Loading Bronze Layer');
-		PRINT('=======================================');
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
 
-		PRINT('=======================================');
-		PRINT('Loading CRM Table');
-		PRINT('=======================================');
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
 
-		SET @start_time = GETDATE();
-		PRINT('>> Truncating Table : bronze.crm_cust_info')
-		TRUNCATE TABLE bronze.crm_cust_info; -- used to quickly remove all rows from a table while keeping the table's structure intact so that whenver we execute this query again then the same rows won't be loaded i.e duplicate rows won't be loaded in the data warehouse
-	
-		PRINT ('>> Inserting Data Into : bronze.crm_cust_info')
-		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\sahil\Desktop\New DA-DS\SQL Projects\Data Warehouse Material\datasets\source_crm\cust_info.csv'
-		WITH (
-			FIRSTROW = 2, -- we are skipping the header row i.e row 1 of the csv file coz it contains only the names of columns not actual data
-			FIELDTERMINATOR = ',', -- specifying which separator we have used between the columns
-			TABLOCK  -- locking the entire table to improve performance for large scale operations
-		);
-		SET @end_time = GETDATE();
-		PRINT('>> Load Duration ' + CAST(DATEDIFF(second, @start_time, @end_time)as NVARCHAR ) + ' seconds')
-	--------------x--------------------------------x--------------------------x---------------------------x-------------------------x------------------------------x---------------------------x-------------------------x------------------------x-----------------------x-------------------------x--------------------------------x
-		
-		PRINT('>> Truncating Table : bronze.crm_prd_info')
-		TRUNCATE TABLE bronze.crm_prd_info; -- used to quickly remove all rows from a table while keeping the table's structure intact so that whenver we execute this query again then the same rows won't be loaded i.e duplicate rows won't be loaded in the data warehouse
-	
-		PRINT ('>> Inserting Data Into : bronze.crm_prd_info')
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\sahil\Desktop\New DA-DS\SQL Projects\Data Warehouse Material\datasets\source_crm\prd_info.csv'
-		WITH (
-			FIRSTROW = 2, -- we are skipping the header row i.e row 1 of the csv file coz it contains only the names of columns not actual data
-			FIELDTERMINATOR = ',', -- specifying which separator we have used between the columns
-			TABLOCK  -- locking the entire table to improve performance for large scale operations
-		);
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
-	--------------x--------------------------------x--------------------------x---------------------------x-------------------------x------------------------------x---------------------------x-------------------------x------------------------x-----------------------x-------------------------x--------------------------------x
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
 
-		PRINT('>> Truncating Table : bronze.crm_sales_details')
-		TRUNCATE TABLE bronze.crm_sales_details; -- used to quickly remove all rows from a table while keeping the table's structure intact so that whenver we execute this query again then the same rows won't be loaded i.e duplicate rows won't be loaded in the data warehouse
-	
-		PRINT ('>> Inserting Data Into : bronze.crm_sales_details')
-		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\sahil\Desktop\New DA-DS\SQL Projects\Data Warehouse Material\datasets\source_crm\sales_details.csv'
-		WITH (
-			FIRSTROW = 2, -- we are skipping the header row i.e row 1 of the csv file coz it contains only the names of columns not actual data
-			FIELDTERMINATOR = ',', -- specifying which separator we have used between the columns
-			TABLOCK  -- locking the entire table to improve performance for large scale operations
-		);
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
-		--------------x--------------------------------x--------------------------x---------------------------x-------------------------x------------------------------x---------------------------x-------------------------x------------------------x-----------------------x-------------------------x--------------------------------x
-		PRINT('=======================================');
-		PRINT('Loading ERP Table');
-		PRINT('=======================================');
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
 
-		PRINT('>> Truncating Table : bronze.erp_cust_az12')
-		TRUNCATE TABLE bronze.erp_cust_az12; -- used to quickly remove all rows from a table while keeping the table's structure intact so that whenver we execute this query again then the same rows won't be loaded i.e duplicate rows won't be loaded in the data warehouse
-	
-		PRINT ('>> Inserting Data Into : bronze.erp_cust_az12')
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\sahil\Desktop\New DA-DS\SQL Projects\Data Warehouse Material\datasets\source_erp\cust_az12.csv'
-		WITH (
-			FIRSTROW = 2, -- we are skipping the header row i.e row 1 of the csv file coz it contains only the names of columns not actual data
-			FIELDTERMINATOR = ',', -- specifying which separator we have used between the columns
-			TABLOCK  -- locking the entire table to improve performance for large scale operations
-		);
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-	--------------x--------------------------------x--------------------------x---------------------------x-------------------------x------------------------------x---------------------------x-------------------------x------------------------x-----------------------x-------------------------x--------------------------------x
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
 
-		PRINT('>> Truncating Table : bronze.erp_loc_a101')
-		TRUNCATE TABLE bronze.erp_loc_a101; -- used to quickly remove all rows from a table while keeping the table's structure intact so that whenver we execute this query again then the same rows won't be loaded i.e duplicate rows won't be loaded in the data warehouse
-	
-		PRINT ('>> Inserting Data Into : bronze.erp_loc_a101')
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\sahil\Desktop\New DA-DS\SQL Projects\Data Warehouse Material\datasets\source_erp\loc_a101.csv'
-		WITH (
-			FIRSTROW = 2, -- we are skipping the header row i.e row 1 of the csv file coz it contains only the names of columns not actual data
-			FIELDTERMINATOR = ',', -- specifying which separator we have used between the columns
-			TABLOCK  -- locking the entire table to improve performance for large scale operations
-		);
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
 
-	--------------x--------------------------------x--------------------------x---------------------------x-------------------------x------------------------------x---------------------------x-------------------------x------------------------x-----------------------x-------------------------x--------------------------------x
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
 
-		PRINT('>> Truncating Table : bronze.erp_px_cat_g1v2')
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2; -- used to quickly remove all rows from a table while keeping the table's structure intact so that whenver we execute this query again then the same rows won't be loaded i.e duplicate rows won't be loaded in the data warehouse
-	
-		PRINT ('>> Inserting Data Into : bronze.erp_px_cat_g1v2')
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\sahil\Desktop\New DA-DS\SQL Projects\Data Warehouse Material\datasets\source_erp\px_cat_g1v2.csv'
-		WITH (
-			FIRSTROW = 2, -- we are skipping the header row i.e row 1 of the csv file coz it contains only the names of columns not actual data
-			FIELDTERMINATOR = ',', -- specifying which separator we have used between the columns
-			TABLOCK  -- locking the entire table to improve performance for large scale operations
-		);
-		SET @batch_start_time = GETDATE()
-		PRINT('==============================================')
-		PRINT('Loading Bronze Layer is completed')
-		PRINT('Total Load Duration: ' + CAST(DATEDIFF(second, @batch_start_time, @batch_end_time) as nvarchar) + ' seconds')
-		PRINT('==============================================')
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 
-	END TRY	
-	BEGIN CATCH
-		PRINT('=============================================')
-		PRINT('Error Occured during Loading Bronze layer')
-		PRINT('Error message : ' + ERROR_MESSAGE())
-		PRINT('Error Number : ' + CAST(ERROR_NUMBER() as NVARCHAR))
-		PRINT('=============================================')
-	END CATCH
-END;
-EXEC bronze.load_bronze
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
